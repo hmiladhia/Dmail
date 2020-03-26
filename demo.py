b04592ce29d99cd67a0929d3c18adb4eb3fe7489
Dhia Hmila <dhiahmila@gmail.com>
2020-03-26 19:32:21 +0100

    updated demo

--- demo.py
+++ demo.py
@@ -12,9 +12,21 @@ This is a **test**
 
 ![test image](tests/another_image.jpg)
 
+| Collumn1 | Collumn2 | Collumn3 |
+| :------: | :------- | -------- |
+| Content1 | Content2 | Content3 |
+
+```python
+def my_function(*args, **kwargs):
+    pass
+```
+
 this is some other text
+
+[^1]: This is a footnote.
+[^2]: This is another footnote.
 """
 
 with Gmail(sender_email, password) as gmail:
     gmail.add_attachment(r"tests\test_image.jpg", "another_name.jpg")
-    gmail.send_message(message, receiver_email, "Subject", subtype='md')
+    gmail.send_message(message, receiver_email, subject="[Dmail] Demo")
