7d1c3eadd261ef7cc65bed946e6983e92b407296
Dhia Hmila <dhiahmila@gmail.com>
2020-03-27 22:12:20 +0100

    deploy

--- deploy.bat
+++ deploy.bat
@@ -26,11 +26,13 @@ GOTO :EOF
 :DEPLOY_PYPI
 SET Test=False
 python -m pip install --user --upgrade twine
+del /Q dist
 python setup.py sdist bdist_wheel && python -m twine upload --skip-existing -u __token__ -p %pypi_token% dist/*
 GOTO :EOF
 
 :DEPLOY_TEST_PYPI
 SET Test=True
 python -m pip install --user --upgrade twine
+del /Q dist
 python setup.py sdist bdist_wheel && python -m twine upload --repository-url https://test.pypi.org/legacy/ --skip-existing -u __token__ -p %pypi_test_token% dist/*
 GOTO :EOF
