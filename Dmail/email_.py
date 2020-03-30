2859eae97ffafaf9ea8f9a00c9109c359fb89b0c
Dhia Hmila <dhiahmila@gmail.com>
2020-03-30 18:16:51 +0200

    bug fix

--- Dmail/email_.py
+++ Dmail/email_.py
@@ -4,3 +4,9 @@ from Dmail.mixin import MarkdownMixin
 
 class Email(SimpleEmail, MarkdownMixin):
     default_subtype = 'md'
+
+    def __init__(self, mail_server, mail_port, sender_email=None, sender_credentials=None, mail_use_tls=True,
+                 mail_use_ssl=False, md_extensions=None):
+        super(Email, self).__init__(mail_server=mail_server, mail_port=mail_port, sender_email=sender_email,
+                                    sender_credentials=sender_credentials, mail_use_tls=mail_use_tls,
+                                    mail_use_ssl=mail_use_ssl, md_extensions=md_extensions)
--- Dmail/mixin/markdown_mixin.py
+++ Dmail/mixin/markdown_mixin.py
@@ -1,22 +1,14 @@
-import re
-
 import markdown
 
 from Dmail.mixin import HtmlMixin
 
 
 class MarkdownMixin(HtmlMixin):
-    # _img_regex = re.compile(r'(!\[.*?\]\()(.*?)(\))')
-
-    def __init__(self, mail_server, mail_port, sender_email=None, sender_credentials=None,
-                 mail_use_tls=True, mail_use_ssl=False, md_extensions=None):
-        super(MarkdownMixin, self).__init__(mail_server=mail_server, mail_port=mail_port, sender_email=sender_email,
-                                            sender_credentials=sender_credentials, mail_use_tls=mail_use_tls,
-                                            mail_use_ssl=mail_use_ssl)
+    def __init__(self, sender_email=None, md_extensions=None, *args, **kwargs):
+        super(MarkdownMixin, self).__init__(*args, sender_email=sender_email, **kwargs)
         self._markdown = markdown.Markdown(extensions=md_extensions or ['tables', 'fenced_code', 'footnotes'])
 
     def _process_text(self, text, subtype):
         if subtype == 'md':
-            # text = self._img_regex.sub(self._md_add_img, text)
             text, subtype = self._markdown.convert(text), 'html'
         return super(MarkdownMixin, self)._process_text(text, subtype)
