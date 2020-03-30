35af18b579e168b84df424c2525c02172153d130
Dhia Hmila <dhiahmila@gmail.com>
2020-03-30 22:28:37 +0200

    Added draft creation

--- Dmail/api/gmail_api.py
+++ Dmail/api/gmail_api.py
@@ -10,7 +10,8 @@ from Dmail.mixin import EmailBase, MarkdownMixin, MimeMixin
 
 # If modifying these scopes, delete the file token.pickle.
 # SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+# SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+SCOPES = ['https://www.googleapis.com/auth/gmail.compose']
 
 
 class GmailApiBase(EmailBase):
@@ -57,6 +58,32 @@ class GmailApiBase(EmailBase):
 
 
 class GmailApi(GmailApiBase, MarkdownMixin, MimeMixin):
+    default_subtype = 'md'
+
     def __init__(self, sender_email, token_file, credentials_file=None, md_extensions=None):
         super(GmailApi, self).__init__(sender_email=sender_email, token_file=token_file,
                                        credentials_file=credentials_file, md_extensions=md_extensions)
+
+    def create_draft(self, email_text, to=None, subject=None, cc=None, bcc=None, subtype=None, attachments=None):
+        subtype = subtype or self.default_subtype
+        self._pre_send(email_text, to=to, subject=subject, cc=cc, bcc=bcc, subtype=subtype, attachments=attachments)
+        email_body = self.get_message(email_text, to=to, subject=subject, cc=cc, bcc=bcc,
+                                      subtype=subtype, attachments=attachments)
+        self.createdraft(email_body)
+        self._post_send(email_text, to=to, subject=subject, cc=cc, bcc=bcc, subtype=subtype, attachments=attachments)
+
+    def createdraft(self, message):
+        """Create and insert a draft email. Print the returned draft's message and id.
+
+        Args:
+          message: The body of the email message, including headers.
+
+        Returns:
+          Draft object, including draft id and message meta data.
+        """
+        try:
+            body = {'message': {'raw': base64.urlsafe_b64encode(message.encode()).decode()}}
+            return self.service.users().drafts().create(userId=self.sender_email, body=body).execute()
+        except Exception as error:
+            print('An error occurred: %s' % error)
+            return None
