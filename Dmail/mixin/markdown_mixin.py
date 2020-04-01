f99766d9b2d3429d4504d3bf8397cd5e8b1b347c
Dhia Hmila <dhiahmila@gmail.com>
2020-04-02 01:06:39 +0200

    added default styles

--- Dmail/mixin/markdown_mixin.py
+++ Dmail/mixin/markdown_mixin.py
@@ -1,4 +1,4 @@
-import pathlib
+import os.path as p
 
 import markdown
 
@@ -6,10 +6,11 @@ from Dmail.mixin import HtmlMixin
 
 
 class MarkdownMixin(HtmlMixin):
-    def __init__(self, sender_email, md_extensions=None, styles=None, wrapper_id=None, **kwargs):
+    def __init__(self, sender_email, md_extensions=None, styles=None, default_styles='default', wrapper_id=None, **kwargs):
         super(MarkdownMixin, self).__init__(sender_email=sender_email, **kwargs)
         self._markdown = markdown.Markdown(extensions=md_extensions or ['tables', 'fenced_code', 'footnotes'])
         self.styles = styles or []
+        self.default_styles = default_styles
         self.wrapper_id = wrapper_id
 
     def _process_text(self, text, subtype, **kwargs):
@@ -20,7 +21,7 @@ class MarkdownMixin(HtmlMixin):
         return super(MarkdownMixin, self)._process_text(text, subtype, **kwargs)
 
     def __get_text_with_style(self, text):
-        styles = '\n'.join(self.__get_styles(self.styles))
+        styles = self.__get_styles()
         if styles:
             wrapper_start = f'<div id="{self.wrapper_id}">' if self.wrapper_id else ''
             wrapper_end = f'</div>' if self.wrapper_id else ''
@@ -29,12 +30,18 @@ class MarkdownMixin(HtmlMixin):
         else:
             return text
 
+    def __get_styles(self):
+        return '\n'.join(self.__load_styles(self.default_styles, default=True) + self.__load_styles(self.styles))
+
     @classmethod
-    def __get_styles(cls, styles):
+    def __load_styles(cls, styles, default=False):
         if isinstance(styles, str):
-            return [pathlib.Path(styles).read_text()]
+            path = p.abspath(p.join(p.dirname(__file__), '..', 'styles', f'{styles.lower()}.css') if default else styles)
+            with open(path, 'r') as css_file:
+                style = css_file.read()
+            return [style]
         else:
             s = []
             for style in styles:
-                s.extend(cls.__get_styles(style))
+                s.extend(cls.__load_styles(style))
             return s
